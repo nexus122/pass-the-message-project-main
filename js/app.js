5e4af11dde9f4878fe8543fc158cb7bbffd5be73
nexus122 <juan-pablo122@hotmail.es>
2022-03-25 12:01:01 +0100

    Rama Jquery

--- js/app.js
+++ js/app.js
@@ -1,25 +1,39 @@
 // Tu código aquí
 let Estado = {
-    form:document.querySelector("#message-form"),
-    phrase: document.querySelector("#message"),
-    resp: document.querySelector(".message-content"),
-    alert: document.querySelector(".alert"),
-    escribirLaFrase(){        
-        if(this.phrase.value.trim() == "" || this.phrase.value.trim() == undefined || this.phrase.value.trim() == null){
-            console.log("El mensaje esta vacio");            
-            this.alert.style.display = 'block';
+    form:$("#message-form"),
+    phrase: $("#message"),
+    resp: $(".message-content"),
+    alert: $(".alert"),
+    escribirLaFrase(){
+        if(this.phrase.val().trim() == "" || this.phrase.val().trim() == undefined || this.phrase.val().trim() == null){
+            console.log("El mensaje esta vacio");
+
+            //this.alert.style.display = 'block';
+            this.alert.show();
+
             setTimeout(function(){
-                Estado.alert.style.display = 'none';
+                Estado.alert.hide();
             },1000);
-            return;    
+
+            return;
         }
-        this.resp.innerHTML = this.phrase.value
+
+        //this.resp.innerHTML = this.phrase.value
+        this.resp.text(this.phrase.val());
+
     }
 }
 
+$(Estado.form).submit(function(e){
+    e.preventDefault();    
+    console.log("Se ha recivido el valor");
+    Estado.escribirLaFrase();
+});
+
+/*
 Estado.form.addEventListener('submit', function(e){
-    e.preventDefault();
-    e.stopPropagation();
+    e.preventDefault();    
     console.log("Hemos hecho clic en el botón de submit!")
     Estado.escribirLaFrase();    
 })
+*/
